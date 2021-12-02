4f9875833a3edc52400781b5763d6ae66eb1c10a
Evgeny Kirillov <evgeny.kirillov@alarislabs.com>
2021-12-02 22:20:44 +0300

    02.12.21 Add 4.4.2, 4.4.3 solutions

--- week3/check_general_tree_property/solution.py
+++ week3/check_general_tree_property/solution.py
@@ -1,5 +1,5 @@
 def check_tree_property(root, _from, _to):
-    if _from < data[root][0] < _to:
+    if _from <= data[root][0] < _to:
         left_res = check_tree_property(data[root][1], _from, data[root][0]) if data[root][1] != -1 else True
         if left_res:
             right_res = check_tree_property(data[root][2], data[root][0], _to) if data[root][2] != -1 else True
@@ -18,32 +18,6 @@ if __name__ == '__main__':
     for _ in range(n):
         data.append(list(map(int, list(input()))))
 
-    # CORRECT
-    # n = 5
-    # data = [
-    #     [4, 1, 2],
-    #     [2, 3, 4],
-    #     [5, -1, -1],
-    #     [1, -1, -1],
-    #     [3, -1, -1],
-    # ]
-
-    # INCORRECT
-    # n = 3
-    # data = [
-    #     [1, 1, 2],
-    #     [2, -1, -1],
-    #     [3, -1, -1],
-    # ]
-
-    # INCORRECT
-    # n = 4
-    # data = [
-    #     [2, -1, 1],
-    #     [5, 2, -1],
-    #     [3, -1, 3],
-    #     [5, -1, -1],
-    # ]
     if data:
         if check_tree_property(0, float("-inf"), float("inf")):
             print("CORRECT")
@@ -51,4 +25,3 @@ if __name__ == '__main__':
             print("INCORRECT")
     else:
         print("CORRECT")
-
